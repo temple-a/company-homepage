d0ca84d7566e6ae4393ed9f537843e801c9de97a
[anya temple] <[anya.temple@gmail.com]>
2021-08-01 12:59:00 +0100

    Add js to toggle staff and contact sections

---
+++ script.js
@@ -0,0 +1,25 @@
+let staffKanji = document.getElementById('staff-kanji');
+let contactKanji = document.getElementById('contact-kanji');
+let staff = document.getElementById('staff');
+let contact = document.getElementById('contact');
+
+function toggleStaff() {
+    if (staff.style.display === 'none') {
+        staff.style.display = 'flex';
+    } else {
+        staff.style.display = 'none';
+    }
+}
+
+function toggleContact() {
+    if (contact.style.display === 'none') {
+        contact.style.display = 'flex';
+    } else {
+        contact.style.display = 'none';
+    }
+}
+
+if (window.screen.availWidth < 600) {
+    staffKanji.addEventListener('click', toggleStaff);
+    contactKanji.addEventListener('click', toggleContact);
+}
